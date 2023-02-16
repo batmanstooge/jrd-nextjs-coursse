a42fd11f59d14c83144b037137cb674824f69d82
John Robin Davis <batmanstooge@gmail.com>
2023-02-16 22:26:00 +0530

    Chapter 359

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,8 +1,20 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
 
-    const { title, image, address, description } = data;
+    const client = await MongoClient.connect(
+      "mongodb+srv://batmanstooge:stephan1e@cluster0.ojwnp0x.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+    const result = await meetupsCollection.insertOne(data);
+    console.log(result);
+    client.close();
+    res.status(201).json({ message: "Meetup inserted!" });
   }
 }
 
